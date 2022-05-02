808efe4d78f8569b44844e6781d1b083c6dd6aa1
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2022-05-02 18:10:40 +0300

    Изменил реализацию класса CombineCipher. Добавил Enum для комбинированого шифрования.

--- CipherLab/CombinedСipher.cs
+++ CipherLab/CombinedСipher.cs
@@ -2,25 +2,36 @@
 {
     public class CombinedСipher : ICipher
     {
+        private readonly ICipher _firstCipher;
+        private readonly ICipher _secondCipher;
+
+        public CombinedСipher(CombineEncryptors type)
+        {
+            switch (type)
+            {
+                case CombineEncryptors.ReverseAndNextLetter:
+                    _firstCipher = new LettersReverseCipher();
+                    _secondCipher = new NextLetterCipher();
+                    break;
+                case CombineEncryptors.NextLetterAndReverse:
+                    _firstCipher = new NextLetterCipher();
+                    _secondCipher = new LettersReverseCipher();
+                    break;
+            }
+        }
+
         public string Decode(string decodeStr)
         {
-            var decoderLetterNextLetter = new NextLetterCipher();
-            var str = decoderLetterNextLetter.Encode(decodeStr);
+            var str = _secondCipher.Encode(decodeStr);
 
-            var decoderReverseLetters = new LettersReverseCipher();
-
-            return decoderReverseLetters.Encode(str);
+            return _firstCipher.Encode(str);
         }
 
         public string Encode(string encodeStr)
         {
-            var encoderReverseLetters = new LettersReverseCipher();
-            var str = encoderReverseLetters.Encode(encodeStr);
+            var str = _firstCipher.Encode(encodeStr);
 
-            var encoderLetterNextLetter = new NextLetterCipher();
-
-            return encoderLetterNextLetter.Encode(str);
-
+            return _secondCipher.Encode(str);
         }
     }
 }
